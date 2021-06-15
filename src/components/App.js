8f69b5fa6defc9ca384999162f8dbc779b8fec8e
Bassam <b2dndr1@gmail.com>
2021-06-15 13:39:01 +0200

    setup basic functionality - frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,64 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Web3 from "web3";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { account: "", ethBalance: "0" };
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(window.web3.currentProvider);
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+
+    console.log(this.state.ethBalance);
+  }
+
+  async loadWeb3() {
+    window.addEventListener("load", async () => {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+          // Request account access if needed
+          await window.ethereum.enable();
+          // Acccounts now exposed
+          window.web3.eth.sendTransaction({
+            /* ... */
+          });
+        } catch (error) {
+          // User denied account access...
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        // Acccounts always exposed
+        window.web3.eth.sendTransaction({
+          /* ... */
+        });
+      }
+      // Non-dapp browsers...
+      else {
+        console.log(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+      }
+    });
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+    // console.log(window.web3);
+  }
+
   render() {
     return (
       <div>
@@ -20,25 +76,12 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
+                {/* <a
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
                   rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                ></a> */}
+                <h1>one</h1>
               </div>
             </main>
           </div>
